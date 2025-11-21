9d89495776e73bc66dff56b8df253167d38fb2de
himanshu <hdxt25@gmail.com>
2025-11-21 11:49:59 +0530

    add

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,6 +1,6 @@
-def call(String projectName) {
+def call(String projectName,String ImageTag) {
     sh """
-        IMAGE="$DOCKER_USR/${projectName}:$BUILD_NUMBER"
+        IMAGE="$DOCKER_USR/${projectName}:${ImageTag}"
         echo "Scanning Docker image: \$IMAGE"
         trivy image "\$IMAGE" > trivyimage.txt
     """
