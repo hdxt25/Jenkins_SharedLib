aed2d360413582ac23e01ab15db2280398135986
hdxt25 <hdxt25@gmail.com>
2025-11-21 17:43:54 +0530

    Refactor ecr_image_push.groovy for better logging

--- vars/ecr_image_push.groovy
+++ vars/ecr_image_push.groovy
@@ -1,10 +1,17 @@
-// vars/dockerECR.groovy
+// vars/ecr_image_push.groovy
 def call(String AWS_ECR_REPO_NAME, String AWS_DEFAULT_REGION, String REPOSITORY_URI) {
-    def buildNumber = env.BUILD_NUMBER  // automatically take Jenkins build number
-    sh 
-      """
+    def buildNumber = env.BUILD_NUMBER
+
+    sh """
+        echo "Logging in to AWS ECR..."
         aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REPOSITORY_URI}
-        docker tag "${AWS_ECR_REPO_NAME}" "${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${buildNumber}"
-        docker push "${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${buildNumber}"
-      """
+
+        echo "Tagging image..."
+        docker tag ${AWS_ECR_REPO_NAME} ${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${buildNumber}
+
+        echo "Pushing image to ECR..."
+        docker push ${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${buildNumber}
+
+        echo "ECR push completed for build #${buildNumber}"
+    """
 }
