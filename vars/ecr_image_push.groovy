e77afb63aef58dddd70f6beab09abd0ee6e1edbe
himanshu <hdxt25@gmail.com>
2025-11-21 12:47:14 +0530

    add

--- vars/ecr_image_push.groovy
+++ vars/ecr_image_push.groovy
@@ -3,8 +3,8 @@ def call(String AWS_ECR_REPO_NAME, String AWS_DEFAULT_REGION, String REPOSITORY_
     def buildNumber = env.BUILD_NUMBER  // automatically take Jenkins build number
     sh 
       """
-      aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REPOSITORY_URI}
-      docker tag ${AWS_ECR_REPO_NAME} ${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${buildNumber}
-      docker push ${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${buildNumber}
+        aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REPOSITORY_URI}
+        docker tag "${AWS_ECR_REPO_NAME}" "${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${buildNumber}"
+        docker push "${REPOSITORY_URI}${AWS_ECR_REPO_NAME}:${buildNumber}"
       """
 }
--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,7 +1,12 @@
-def call(String projectName,String ImageTag) {
-    sh """
-        IMAGE="$DOCKER_USR/${projectName}:${ImageTag}"
-        echo "Scanning Docker image: \$IMAGE"
-        trivy image "\$IMAGE" > trivyimage.txt
-    """
+def call(String projectName, String imageTag, String dockerhubuser) {
+    withCredentials([usernamePassword(credentialsId: 'docker', 
+                                     usernameVariable: 'dockerhubuser', 
+                                     passwordVariable: 'dockerhubpass')]) {
+        sh """
+            IMAGE="${dockerhubuser}/${projectName}:${imageTag}"
+            echo "Scanning Docker image: \$IMAGE"
+            trivy image "\$IMAGE" > trivyimage.txt
+        """
+    }
 }
+
