344bd7f8d6d9d0c502522ce85116d5b697ddf0ad
himanshu <hdxt25@gmail.com>
2025-11-21 14:59:40 +0530

    add

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -2,11 +2,13 @@ def call(String projectName, String imageTag, String dockerhubuser) {
     withCredentials([usernamePassword(credentialsId: 'docker', 
                                      usernameVariable: 'dockerhubuser', 
                                      passwordVariable: 'dockerhubpass')]) {
-        sh """
-            IMAGE="${dockerhubuser}/${projectName}:${imageTag}"
-            echo "Scanning Docker image: \$IMAGE"
-            trivy image "\$IMAGE" > trivyimage.txt
-        """
+        withEnv(["PROJECT_NAME=${projectName}", "IMAGE_TAG=${imageTag}", "DOCKER_USER=${dockerhubuser}"]) {
+            sh '''
+                IMAGE="${DOCKER_USER}/${PROJECT_NAME}:${IMAGE_TAG}"
+                echo "Scanning Docker image: $IMAGE"
+                trivy image "$IMAGE" > trivyimage.txt
+            '''
+        }
     }
 }
 
