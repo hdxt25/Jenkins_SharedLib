c300adc714408c25c963005066ac7837cd90b1ce
hdxt25 <hdxt25@gmail.com>
2025-11-21 17:43:34 +0530

    Enhance Docker build script with echo statement
    Added echo statement to indicate the Docker image being built.

--- vars/ecr_image_build.groovy
+++ vars/ecr_image_build.groovy
@@ -1,7 +1,10 @@
-// vars/dockerECR.groovy
+// vars/docker_build.groovy
 def call(String AWS_ECR_REPO_NAME) {
-    
-        sh 'docker system prune -f'
-        sh 'docker container prune -f'
-        sh "docker build -t ${AWS_ECR_REPO_NAME} ."
-    }
+    sh 'docker system prune -f'
+    sh 'docker container prune -f'
+
+    sh """
+        echo "Building Docker image: ${AWS_ECR_REPO_NAME}"
+        docker build -t ${AWS_ECR_REPO_NAME} .
+    """
+}
